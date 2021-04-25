991794ea04b41377d433cfff4bafd7976cc9c31a
Eli Aloni <elix22@gmail.com>
2021-04-25 11:24:26 +0300

    set mosue visible in Chat.cs and SceneReplication.cs

--- FeatureSamples/Source/Chat/Chat.cs
+++ FeatureSamples/Source/Chat/Chat.cs
@@ -129,6 +129,8 @@ namespace Chat
 
             // No viewports or scene is defined. However, the default zone's fog color controls the fill color
             Renderer.DefaultZone.FogColor = new Color(0.0f, 0.0f, 0.1f);
+
+            Input.SetMouseVisible(true);
         }
 
         void SubscribeToEvents()
